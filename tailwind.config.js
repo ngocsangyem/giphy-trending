548905fa432fb98d40e40d6c618701f2a66f2fd3
Sangg <ngocsangyem@gmail.com>
2022-03-26 21:38:51 +0700

    :lipstick:  add class overlay

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
 	content: ['./index.html', './src/**/*.{jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'overlay': 'rgba(0, 0, 0, 0.3)',
+			},
+		},
 	},
 	plugins: [],
 };
