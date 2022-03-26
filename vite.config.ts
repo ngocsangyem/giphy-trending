d0b7d9d7de2ff5a1b0b786facea1b66f2158b97b
Sangg <ngocsangyem@gmail.com>
2022-03-26 13:28:39 +0700

    :wrench:  update alias paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+	plugins: [react()],
+	alias: {
+		'@/': resolve(__dirname, './src'),
+		'@/components': resolve(__dirname, './src/components'),
+		'@/styles': resolve(__dirname, './src/styles'),
+		'@/common': resolve(__dirname, './src/components/common'),
+		'@/assets': resolve(__dirname, './src/assets'),
+		'@/pages': resolve(__dirname, './src/pages'),
+		'@/routes': resolve(__dirname, './src/routes'),
+		'@/hooks': resolve(__dirname, './src/hooks'),
+		'@/store': resolve(__dirname, './src/store'),
+		'@/services': resolve(__dirname, './src/services'),
+	},
+});
