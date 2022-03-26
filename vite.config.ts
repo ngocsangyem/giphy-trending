0d15a4c83161dd8b13019f2ac1977785798e19f8
Sangg <ngocsangyem@gmail.com>
2022-03-26 18:34:16 +0700

    :wrench:  update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,18 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	alias: {
-		'@/': resolve(__dirname, './src'),
-		'@/components': resolve(__dirname, './src/components'),
-		'@/styles': resolve(__dirname, './src/styles'),
-		'@/common': resolve(__dirname, './src/components/common'),
-		'@/assets': resolve(__dirname, './src/assets'),
-		'@/pages': resolve(__dirname, './src/pages'),
-		'@/routes': resolve(__dirname, './src/routes'),
-		'@/hooks': resolve(__dirname, './src/hooks'),
-		'@/store': resolve(__dirname, './src/store'),
-		'@/services': resolve(__dirname, './src/services'),
+	resolve: {
+		alias: {
+			'@/': resolve(__dirname, './src'),
+			'@/components': resolve(__dirname, './src/components'),
+			'@/styles': resolve(__dirname, './src/styles'),
+			'@/common': resolve(__dirname, './src/components/common'),
+			'@/assets': resolve(__dirname, './src/assets'),
+			'@/pages': resolve(__dirname, './src/pages'),
+			'@/routes': resolve(__dirname, './src/routes'),
+			'@/hooks': resolve(__dirname, './src/hooks'),
+			'@/store': resolve(__dirname, './src/store'),
+			'@/services': resolve(__dirname, './src/services'),
+		},
 	},
 });
