f2751d294bae5e402a7c4465d1e2947a5959f9d0
Sangg <ngocsangyem@gmail.com>
2022-04-02 13:55:57 +0700

    :fire:  remove like function

--- src/components/Giphy/GiphyItemOverlay/GiphyItemOverlay.tsx
+++ src/components/Giphy/GiphyItemOverlay/GiphyItemOverlay.tsx
@@ -1,5 +1,4 @@
 import { GifOverlayProps } from '@giphy/react-components';
-import { forwardRef, useState } from 'react';
 import styles from './GiphyItemOverlay.module.scss';
 
 const GiphyItemOverlay = ({ gif, isHovered, onClick }: GifOverlayProps) => {
@@ -9,21 +8,6 @@ const GiphyItemOverlay = ({ gif, isHovered, onClick }: GifOverlayProps) => {
 				isHovered ? 'opacity-100' : 'opacity-0'
 			}`}
 		>
-			<div
-				className={`bg-transparent border-0 p-0 absolute top-2 right-2 z-10 flex justify-center w-8 h-8 ${styles['like-btn']}`}
-				role="button"
-			>
-				<div className="relative flex justify-center w-8 h-8">
-					<input id={`toggle-heart-${gif.id}`} type="checkbox" className='cursor-pointer absolute left-0 w-full h-full opacity-0 z-10 toggle-input' />
-					<label
-						htmlFor={`toggle-heart-${gif.id}`}
-						aria-label="like"
-						className="toggle-label"
-					>
-						❤
-					</label>
-				</div>
-			</div>
 			<div className="absolute inset-0 flex bg-overlay items-end justify-center p-2 text-white font-semibold">
 				<span>{gif.title}</span>
 			</div>
