0c370917c53da203a4aada896397926ae4c15d94
Sangg <ngocsangyem@gmail.com>
2022-04-02 13:59:21 +0700

    :fire:  remove nav

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,16 +7,6 @@ const Header = () => {
 			<div className="my-10 w-40 mx-auto">
 				<img src={logo} alt="giphy logo" />
 			</div>
-			<nav className="mb-5">
-				<ul className="flex justify-center">
-					<li>
-						<CustomLink to="/">Home</CustomLink>
-					</li>
-					<li>
-						<CustomLink to="/favorite">Favorite</CustomLink>
-					</li>
-				</ul>
-			</nav>
 		</header>
 	);
 };
