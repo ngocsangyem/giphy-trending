744c524917f39d797c6f79cd7642bb515de5d4b7
Sangg <ngocsangyem@gmail.com>
2022-04-02 13:58:23 +0700

    :fire:  remove favorite

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { Fragment } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
-import GiphyFavorite from './pages/GiphyFavorite/GiphyFavorite';
 import Home from './pages/Home/Home';
 import { SearchContextProvider } from '@/context/search-context/search-context';
 
@@ -13,7 +12,6 @@ function App() {
 			<SearchContextProvider apiKey={import.meta.env.VITE_GIPHY_API as string}>
 				<Routes>
 					<Route path="/" element={<Home />} />
-					<Route path="favorite" element={<GiphyFavorite />} />
 				</Routes>
 			</SearchContextProvider>
 		</Fragment>
